d59719ae7bb66d2fdf095006e346d23b194ea8a3
GuitarCYQ <1159543426@qq.com>
2020-10-21 08:02:01 +0000

    邮箱验证

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Auth\MustVerifyEmail as MustVerifyEmailTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use Notifiable;
+    use Notifiable,MustVerifyEmailTrait;
+
+    /**
+     *使用MustVerifyEmailTrait
+     * hasVerifiedEmail() 检测用户 Email 是否已认证；
+     * markEmailAsVerified() 将用户标示为已认证；
+     * sendEmailVerificationNotification() 发送 Email 认证的消息通知，触发邮件的发送；
+     * getEmailForVerification() 获取发送邮件地址，提供这个接口允许你自定义邮箱字段。
+     */
 
     /**
      * The attributes that are mass assignable.
