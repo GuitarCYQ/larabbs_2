7eb1233c6ebc88cb7b184c7dc51c38b37d792ee6
GuitarCYQ <1159543426@qq.com>
2020-10-23 05:09:42 +0000

    裁剪头像

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -35,12 +35,14 @@ class UserRequest extends FormRequest
     public function messages()
     {
         return [
+            'avatar.mimes'  =>  '头像必须是jpeg，bmp，png，fig格式的图片',
+            'avatar.dimensions' =>  '图片的清晰度不够，宽和高需要208px以上',
             'name.unique'   =>  '用户名已被占用，请重新填写',
             'name.regex'    =>  '用户名只支持英文、数字、横杠和下划线。',
             'name.between'  =>  '用户名必须介于3-25个字符之间。',
             'name.required' =>  '用户名不能为空',
-            'avatar.mimes'  =>  '头像必须是jpeg，bmp，png，fig格式的图片',
-            'avatar.dimensions' =>  '图片的清晰度不够，宽和高需要208px以上',
+
         ];
     }
 }
+
