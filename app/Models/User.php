e60a70ba34243a0e6af8c9e777a96fda543d2586
GuitarCYQ <1159543426@qq.com>
2020-10-27 09:00:04 +0000

    用户管理后台

--- app/Models/User.php
+++ app/Models/User.php
@@ -96,4 +96,27 @@ class User extends Authenticatable implements MustVerifyEmailContract
         $this->unreadNotifications->markAsRead();
     }
 
+    public function setPasswordAttribute($value)
+    {
+        //如果值的长度等于60即认为值已经做过加密的情况
+        if (strlen($value) != 60){
+            //不等于60 做加密处理
+            $value = bcrypt($value);
+        }
+
+        $this->attributes['password'] = $value;
+    }
+
+    public function setAvatarAttribute($path)
+    {
+        //如果不是‘http’ 子串开头，那就是从后台上传的，需要补全url
+        if ( ! \Str::startsWith($path,'http')) {
+
+            //拼接完整的URL
+            $path = config('app.url') . "uploads/images/avatars/$path";
+        }
+
+        $this->attributes['avatar'] = $path;
+    }
+
 }
